2a722fdb8b2c3a659555db99e2e27ef24e527eb7
krishna <krishna.kondoju007@gmail.com>
2018-02-08 22:52:43 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
-import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Input, { InputLabel } from 'material-ui/Input';
+import { FormControl, FormHelperText } from 'material-ui/Form';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-      </div>
-    );
-  }
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+    },
+});
+
+class ComposedTextField extends React.Component {
+    state = {
+        name: 'Leave Comments Here',
+    };
+
+    handleChange = event => {
+        this.setState({ name: event.target.value });
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.container}>
+                <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="name-simple">Comments</InputLabel>
+                    <Input id="name-simple" value={this.state.name} onChange={this.handleChange} />
+                </FormControl>
+            </div>
+        );
+    }
 }
 
-export default App;
+ComposedTextField.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ComposedTextField);
